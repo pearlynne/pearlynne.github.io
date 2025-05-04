043a56c04e8b8d5fe2d7cd8d3419800670a216c9
pearlynne <pearlynne@peapea-easley-24.local>
2025-05-04 16:33:02 -0700

    Move unused components to archive

---
+++ src/__archive__/resume.jsx
@@ -0,0 +1,14 @@
+import React from "react";
+import Body from "../components/Body";
+
+function Resume() {
+  return (
+    <Body>
+				<iframe
+					src="https://docs.google.com/document/d/e/2PACX-1vTgYRnD-ZsMrUR35PMlYseRUDVn2eC3gJ3mnsX7iHFlzpHLFK9_R-un0A0ZlboWkYAL062cWDg6IhqI/pub?embedded=true"
+				></iframe>
+		</Body>
+  );
+}
+
+export default Resume;
