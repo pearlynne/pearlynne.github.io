2ebf5935602a721b7aaf7e5f500f63e1acc1649d
pearlynne <pearlynne@peapea-easley-24.local>
2025-05-08 10:26:21 -0700

    Add GA with vite-plugin-radar

--- vite.config.js
+++ vite.config.js
@@ -1,10 +1,18 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 import path from 'path';
+import { VitePluginRadar } from 'vite-plugin-radar'
 
 export default defineConfig({
 	base:"/",
-  plugins: [react()],
+  plugins: [
+    react(),
+    VitePluginRadar({
+      analytics: {
+        id: 'G-VXNR7RTN2T', 
+      },
+    })
+  ],
 	resolve: {
 		alias: {
 			'@': path.resolve(__dirname, 'src'),
