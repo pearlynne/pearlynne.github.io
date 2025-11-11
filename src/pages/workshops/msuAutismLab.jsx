8f34ac35dd328da1c83c3b4447098ea53d991e3b
pearlynne <pearlynne@peapea-easley-24.local>
2025-11-10 21:20:29 -0800

    Update name of links to include date

--- src/pages/workshops/msuAutismLab.jsx
+++ src/pages/workshops/msuAutismLab.jsx
@@ -1,8 +1,8 @@
 import Resources from "@/components/Resources.jsx";
 
 const resourceLinks = [
-  { name: "Clipped Recording", url: "https://www.dropbox.com/scl/fi/4udf7s20bedq4ftpunm4l/IntroDataStatsRes_MSU.mp4?rlkey=3l2v5prj48pdnfu078uezeqid&st=egxm68o0&dl=0", icon: "📹" },
-  { name: "Slides", url: "https://www.dropbox.com/scl/fi/jzupdfj4ocebhz9iyrbd1/IntroDataStatsRes_MSUAutismLab.pdf?rlkey=xfq7bomd4q3wmo0lx3ci4bmcu&st=q35tt4wp&dl=0", icon: "📑" },
+  { name: "Clipped Recording - Nov 7th, 2025", url: "https://www.dropbox.com/scl/fi/4udf7s20bedq4ftpunm4l/IntroDataStatsRes_MSU.mp4?rlkey=3l2v5prj48pdnfu078uezeqid&st=egxm68o0&dl=0", icon: "📹" },
+  { name: "Slides PDF", url: "https://www.dropbox.com/scl/fi/jzupdfj4ocebhz9iyrbd1/IntroDataStatsRes_MSUAutismLab.pdf?rlkey=xfq7bomd4q3wmo0lx3ci4bmcu&st=q35tt4wp&dl=0", icon: "📑" },
 ];
 
 const MSUAutismLabResources = () => (
