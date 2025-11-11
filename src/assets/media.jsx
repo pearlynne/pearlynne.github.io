a9fe3669d333a449ec0f3065ed09c68aad320953
pearlynne <pearlynne@peapea-easley-24.local>
2025-11-10 18:06:15 -0800

    Create component for assets

---
+++ src/assets/media.jsx
@@ -0,0 +1,53 @@
+import DS01_1 from "@/assets/DS01_EACSF_1.png";
+import DS01_2 from "@/assets/DS01_EACSF_2.png";
+import DS01_3 from "@/assets/DS01_EACSF_3.png";
+
+import DS02_1 from "@/assets/DS02_GS_1.jpeg";
+import DS02_2 from "@/assets/DS02_GS_2.jpeg";
+
+import DS03_1 from "@/assets/DS03_E4_1.png";
+import DS03_2 from "@/assets/DS03_E4_2.png";
+
+import DS04_1 from "@/assets/DS04_SleepCons.png";
+
+import DS05_1 from "@/assets/DS05_Webscrape.png";
+
+import SE01_VID from "@/assets/SE01_To-do-list.mov";
+import SE01_1 from "@/assets/SE01_To-do-list.png";
+
+import SE02_1 from "@/assets/SE02_HealthAPI_1.png";
+import SE02_2 from "@/assets/SE02_HealthAPI_2.png";
+import SE02_3 from "@/assets/SE02_HealthAPI_3.png";
+import SE02_4 from "@/assets/SE02_HealthAPI_4.png";
+
+import SE03_1 from "@/assets/SE03_Ecommerce_1.png";
+import SE03_2 from "@/assets/SE03_Ecommerce_2.png";
+import SE03_3 from "@/assets/SE03_Ecommerce_3.png";
+import SE03_4 from "@/assets/SE03_Ecommerce_4.png";
+import SE03_5 from "@/assets/SE03_Ecommerce_5.png";
+
+import SE04_1 from "@/assets/SE04_Portfolio.png";
+
+export const media = {
+  DS01: [DS01_1, DS01_2, DS01_3],
+  DS02: [DS02_1, DS02_2],
+  DS03: [DS03_1, DS03_2],
+  DS04: [DS04_1],
+	DS05: [DS05_1],
+  SE01: [SE01_1, SE01_VID],
+  SE02: [SE02_1, SE02_2, SE02_3, SE02_4],
+  SE03: [SE03_1, SE03_2, SE03_3, SE03_4, SE03_5],
+	SE04: [SE04_1],
+};
+
+export const projectsMeta = {
+  DS01: { title: "Extra Axial CSF", type: "data" },
+  DS02: { title: "Glymphatic system", type: "data" },
+  DS03: { title: "Electrodermal Activity", type: "data" },
+  DS04: { title: "Sleep Construct", type: "data" },
+  DS05: { title: "Web Scraping", type: "data" },
+  SE01: { title: "To-Do List App", type: "software" },
+  SE02: { title: "Health Tracking API", type: "software" },
+  SE03: { title: "E-commerce Platform", type: "software" },
+  SE04: { title: "Portfolio Site", type: "software" },
+};
