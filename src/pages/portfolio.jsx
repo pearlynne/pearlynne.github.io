3d60cb0785cb610bcec6c61f51d1c7411391b71d
pearlynne <pearlynne@peapea-easley-24.local>
2025-11-10 19:09:35 -0800

    Correct casing for Portfolio styles to resolve build failure

--- src/pages/portfolio.jsx
+++ src/pages/portfolio.jsx
@@ -1,6 +1,6 @@
 import React from "react";
 import { Link } from "react-router-dom";
-import styles from "../styles/Portfolio.module.css";
+import styles from "../styles/portfolio.module.css";
 import { media, projectsMeta } from "@/assets/media";
 
 const Portfolio = () => {
