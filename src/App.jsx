0ccb167f5fb78019c9200ad5a86c8897a7d041a8
pearlynne <pearlynne@peapea-easley-24.local>
2025-11-10 21:07:22 -0800

    Add resource page

--- src/App.jsx
+++ src/App.jsx
@@ -8,6 +8,7 @@ import Publications from "./pages/publications";
 import ProjectTemplate from "./components/ProjectTemplate";
 import ScrollToTop from "./components/ScrollToTop";
 import Layout from "./components/Layout";
+import MSUAutismLabResources from "./pages/workshops/msuAutismLab";
 
 function App() {
   return (
@@ -21,6 +22,7 @@ function App() {
         <Route path="/publications" element={<Publications />} />
 				<Route path="/consulting" element={<Consulting />} />
 				<Route path="/projects/:id" element={<ProjectTemplate />} /> 
+				<Route path="/MSUAutismLab" element={<MSUAutismLabResources/>} />
 				</Route>
       </Routes>
       <Footer />
